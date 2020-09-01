588f9a7b5ddbe86f82337d57d887f91995ba7d36
Eliopausa <62565767+Eliopausa@users.noreply.github.com>
2020-09-01 17:21:25 +0300

    Update 2.4_8.py

--- Stepik_Selenium_Python/Module_2/2.4_8.py
+++ Stepik_Selenium_Python/Module_2/2.4_8.py
@@ -8,7 +8,6 @@ from selenium.webdriver.support import expected_conditions as EC
 with webdriver.Chrome() as browser:
     browser.implicitly_wait(10)
     browser.get("http://suninjuly.github.io/explicit_wait2.html")
-    price_counter = browser.find_element_by_css_selector("#price")
     button_book = browser.find_element_by_css_selector(".btn")
     price_event = WebDriverWait(browser,10).until(EC.text_to_be_present_in_element((By.ID, "price"), str(100)))
     button_book.click()
